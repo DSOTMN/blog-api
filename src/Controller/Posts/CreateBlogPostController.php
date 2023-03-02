a57d13c0605d6b31b5643feeb8f5b879d1c24411
Aladin Hadzic <aladinhadziic@gmail.com>
2023-03-02 15:04:48 +0100

    Implementing slim with DI to Posts Controller

--- src/Controller/Posts/CreateBlogPostController.php
+++ src/Controller/Posts/CreateBlogPostController.php
@@ -1,19 +1,32 @@
 <?php
 
-namespace BlogRestApi\Controller;
+namespace BlogRestApi\Controller\Posts;
 
-use BlogRestApi\Connection;
 use BlogRestApi\Repository\PostRepository\PostRepositoryPdo;
+use DI\DependencyException;
+use DI\NotFoundException;
 use Laminas\Diactoros\Response\JsonResponse;
+use PDO;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
+use DI\Container;
 
 class CreateBlogPostController
 {
+    private PDO $pdo;
+
+    /**
+     * @throws DependencyException
+     * @throws NotFoundException
+     */
+    public function __construct(Private Container $container)
+    {
+        $this->pdo = $container->get('db');
+    }
+
     public function __invoke(ServerRequestInterface $request):ResponseInterface
     {
-        $connection = Connection::connect();
-        $newPost = new PostRepositoryPdo($connection);
+        $newPost = new PostRepositoryPdo($this->pdo);
         $data = json_decode($request->getBody()->getContents(), true);
         $id = $newPost->store($data);
 
