90437b1227469e2003f4428eb24138b7bd70e5ff
Aladin Hadzic <aladinhadziic@gmail.com>
2023-03-10 14:32:16 +0100

    Fix sql for retrieving single post

--- src/Repository/PostRepository/PostRepositoryPdo.php
+++ src/Repository/PostRepository/PostRepositoryPdo.php
@@ -32,10 +32,32 @@ class PostRepositoryPdo implements PostRepository
 
     public function get($id): array
     {
-        $stmt = $this->connection->prepare('SELECT * FROM posts WHERE id=:id');
+        $stmt = $this->connection->prepare(<<<SQL
+            SELECT posts.*, categories.*
+            FROM posts_categories
+            JOIN categories
+            ON posts_categories.id_category = categories.category_id
+            LEFT JOIN posts
+            ON posts_categories.id_post = posts.post_id
+            WHERE posts.post_id = :id
+        SQL);
         $stmt->bindParam(':id', $id);
         $stmt->execute();
-        return $stmt->fetch(\PDO::FETCH_ASSOC);
+        $result = $stmt->fetch(\PDO::FETCH_ASSOC);
+        return [
+            'id' => $result['post_id'],
+            'title' => $result['title'],
+            'slug' => $result['slug'],
+            'content' => $result['content'],
+            'thumbnail' => $result['thumbnail'],
+            'author' => $result['author'],
+            'posted_at' => $result['posted_at'],
+            'categories' => [
+
+                'id' => $result['category_id'],
+                'name' => $result['name'],
+            ]
+        ];
     }
 
     public function all(): array
@@ -50,7 +72,28 @@ class PostRepositoryPdo implements PostRepository
         SQL);
 
 
+
         $stmt->execute();
+
+        $posts = [];
+
+        while($row = $stmt->fetch(\PDO::FETCH_ASSOC)){
+            $posts[] = [
+                'id' => $row['post_id'],
+                'title' => $row['title'],
+                'slug' => $row['slug'],
+                'content' => $row['content'],
+                'thumbnail' => $row['thumbnail'],
+                'author' => $row['author'],
+                'posted_at' => $row['posted_at'],
+                'categories' => [
+                    'id' => $row['id_category'],
+                    'name' => $row['name']
+                ]
+            ];
+        }
+
+        /*
         $posts = $stmt->fetchAll(\PDO::FETCH_BOTH);
         $postCollection = [];
         foreach($posts as $row){
@@ -68,12 +111,13 @@ class PostRepositoryPdo implements PostRepository
                 ],
             ];
         }
-        return $postCollection;
+        */
+        return $posts;
     }
 
     public function remove(string $id): string
     {
-        $stmt = $this->connection->prepare('DELETE FROM posts WHERE id=:id');
+        $stmt = $this->connection->prepare('DELETE FROM posts WHERE post_id=:id');
         $stmt->bindParam(':id', $id);
         $stmt->execute();
         return "$id deleted";
@@ -81,7 +125,7 @@ class PostRepositoryPdo implements PostRepository
 
     public function update(string $id, array $data): string
     {
-        $stmt = $this->connection->prepare('UPDATE posts SET title=:title, content=:content, thumbnail=:thumbnail WHERE id=:id');
+        $stmt = $this->connection->prepare('UPDATE posts SET title=:title, content=:content, thumbnail=:thumbnail WHERE post_id=:id');
 
         $stmt->execute([
             ':title' => $data['title'],
